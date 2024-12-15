c2be533173c697c5cf67720b486ac7425109af7d
Arigato666 <1330270249@qq.com>
2024-12-15 10:31:28 +0800

    2

--- build/Desktop_Qt_6_5_3_MinGW_64_bit-Debug/debug/moc_gametimer.cpp
+++ build/Desktop_Qt_6_5_3_MinGW_64_bit-Debug/debug/moc_gametimer.cpp
@@ -41,15 +41,19 @@ static constexpr auto qt_meta_stringdata_CLASSGameTimerENDCLASS = QtMocHelpers::
     "GameTimer",
     "timeExpired",
     "",
+    "timeUpdated",
+    "remainingSeconds",
     "updateTime"
 );
 #else  // !QT_MOC_HAS_STRING_DATA
 struct qt_meta_stringdata_CLASSGameTimerENDCLASS_t {
-    uint offsetsAndSizes[8];
+    uint offsetsAndSizes[12];
     char stringdata0[10];
     char stringdata1[12];
     char stringdata2[1];
-    char stringdata3[11];
+    char stringdata3[12];
+    char stringdata4[17];
+    char stringdata5[11];
 };
 #define QT_MOC_LITERAL(ofs, len) \
     uint(sizeof(qt_meta_stringdata_CLASSGameTimerENDCLASS_t::offsetsAndSizes) + ofs), len 
@@ -58,11 +62,15 @@ Q_CONSTINIT static const qt_meta_stringdata_CLASSGameTimerENDCLASS_t qt_meta_str
         QT_MOC_LITERAL(0, 9),  // "GameTimer"
         QT_MOC_LITERAL(10, 11),  // "timeExpired"
         QT_MOC_LITERAL(22, 0),  // ""
-        QT_MOC_LITERAL(23, 10)   // "updateTime"
+        QT_MOC_LITERAL(23, 11),  // "timeUpdated"
+        QT_MOC_LITERAL(35, 16),  // "remainingSeconds"
+        QT_MOC_LITERAL(52, 10)   // "updateTime"
     },
     "GameTimer",
     "timeExpired",
     "",
+    "timeUpdated",
+    "remainingSeconds",
     "updateTime"
 };
 #undef QT_MOC_LITERAL
@@ -75,21 +83,23 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSGameTimerENDCLASS[] = {
       11,       // revision
        0,       // classname
        0,    0, // classinfo
-       2,   14, // methods
+       3,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
-       1,       // signalCount
+       2,       // signalCount
 
  // signals: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    0,   26,    2, 0x06,    1 /* Public */,
+       1,    0,   32,    2, 0x06,    1 /* Public */,
+       3,    1,   33,    2, 0x06,    2 /* Public */,
 
  // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       3,    0,   27,    2, 0x08,    2 /* Private */,
+       5,    0,   36,    2, 0x08,    4 /* Private */,
 
  // signals: parameters
     QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    4,
 
  // slots: parameters
     QMetaType::Void,
@@ -108,6 +118,9 @@ Q_CONSTINIT const QMetaObject GameTimer::staticMetaObject = { {
         QtPrivate::TypeAndForceComplete<GameTimer, std::true_type>,
         // method 'timeExpired'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'timeUpdated'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
         // method 'updateTime'
         QtPrivate::TypeAndForceComplete<void, std::false_type>
     >,
@@ -121,7 +134,8 @@ void GameTimer::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, v
         (void)_t;
         switch (_id) {
         case 0: _t->timeExpired(); break;
-        case 1: _t->updateTime(); break;
+        case 1: _t->timeUpdated((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 2: _t->updateTime(); break;
         default: ;
         }
     } else if (_c == QMetaObject::IndexOfMethod) {
@@ -133,8 +147,14 @@ void GameTimer::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, v
                 return;
             }
         }
+        {
+            using _t = void (GameTimer::*)(int );
+            if (_t _q_method = &GameTimer::timeUpdated; *reinterpret_cast<_t *>(_a[1]) == _q_method) {
+                *result = 1;
+                return;
+            }
+        }
     }
-    (void)_a;
 }
 
 const QMetaObject *GameTimer::metaObject() const
@@ -156,13 +176,13 @@ int GameTimer::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 2)
+        if (_id < 3)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 2;
+        _id -= 3;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 2)
+        if (_id < 3)
             *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
-        _id -= 2;
+        _id -= 3;
     }
     return _id;
 }
@@ -171,5 +191,12 @@ int GameTimer::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 void GameTimer::timeExpired()
 {
     QMetaObject::activate(this, &staticMetaObject, 0, nullptr);
+}
+
+// SIGNAL 1
+void GameTimer::timeUpdated(int _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
+    QMetaObject::activate(this, &staticMetaObject, 1, _a);
 }
 QT_WARNING_POP
