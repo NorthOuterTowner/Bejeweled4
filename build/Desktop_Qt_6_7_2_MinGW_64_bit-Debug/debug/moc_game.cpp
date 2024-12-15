d3c62e7624638a7e15cf409b457fa1eb22c68912
ZXY7112 <144028624+ZXY7112@users.noreply.github.com>
2024-12-15 16:58:22 +0800

    提升了暂停界面，但重新开始游戏有问题

--- build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/debug/moc_game.cpp
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/debug/moc_game.cpp
@@ -39,13 +39,14 @@ constexpr auto qt_meta_stringdata_CLASSGameENDCLASS = QtMocHelpers::stringData(
     "Game",
     "eliminateAgainSignal",
     "",
-    "returnMainwindow",
     "initEndSignal",
+    "returnMainwindow",
     "initEnd",
     "onDropAnimationFinished",
     "onEliminateAgain",
-    "on_pushButton_clicked",
-    "on_pushButton_3_clicked"
+    "on_pushButton_3_clicked",
+    "on_returnFromPauseToMainMenu",
+    "resume"
 );
 #else  // !QT_MOC_HAS_STRINGDATA
 #error "qtmochelpers.h not found or too old."
@@ -58,7 +59,7 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSGameENDCLASS[] = {
       12,       // revision
        0,       // classname
        0,    0, // classinfo
-       8,   14, // methods
+       9,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -66,16 +67,17 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSGameENDCLASS[] = {
        3,       // signalCount
 
  // signals: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    0,   62,    2, 0x06,    1 /* Public */,
-       3,    0,   63,    2, 0x06,    2 /* Public */,
-       4,    0,   64,    2, 0x06,    3 /* Public */,
+       1,    0,   68,    2, 0x06,    1 /* Public */,
+       3,    0,   69,    2, 0x06,    2 /* Public */,
+       4,    0,   70,    2, 0x06,    3 /* Public */,
 
  // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       5,    0,   65,    2, 0x08,    4 /* Private */,
-       6,    0,   66,    2, 0x08,    5 /* Private */,
-       7,    0,   67,    2, 0x08,    6 /* Private */,
-       8,    0,   68,    2, 0x08,    7 /* Private */,
-       9,    0,   69,    2, 0x08,    8 /* Private */,
+       5,    0,   71,    2, 0x08,    4 /* Private */,
+       6,    0,   72,    2, 0x08,    5 /* Private */,
+       7,    0,   73,    2, 0x08,    6 /* Private */,
+       8,    0,   74,    2, 0x08,    7 /* Private */,
+       9,    0,   75,    2, 0x08,    8 /* Private */,
+      10,    0,   76,    2, 0x08,    9 /* Private */,
 
  // signals: parameters
     QMetaType::Void,
@@ -83,6 +85,7 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSGameENDCLASS[] = {
     QMetaType::Void,
 
  // slots: parameters
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -103,9 +106,9 @@ Q_CONSTINIT const QMetaObject Game::staticMetaObject = { {
         QtPrivate::TypeAndForceComplete<Game, std::true_type>,
         // method 'eliminateAgainSignal'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        // method 'returnMainwindow'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>,
         // method 'initEndSignal'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'returnMainwindow'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
         // method 'initEnd'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
@@ -113,9 +116,11 @@ Q_CONSTINIT const QMetaObject Game::staticMetaObject = { {
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
         // method 'onEliminateAgain'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        // method 'on_pushButton_clicked'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>,
         // method 'on_pushButton_3_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_returnFromPauseToMainMenu'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'resume'
         QtPrivate::TypeAndForceComplete<void, std::false_type>
     >,
     nullptr
@@ -128,13 +133,14 @@ void Game::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void *
         (void)_t;
         switch (_id) {
         case 0: _t->eliminateAgainSignal(); break;
-        case 1: _t->returnMainwindow(); break;
-        case 2: _t->initEndSignal(); break;
+        case 1: _t->initEndSignal(); break;
+        case 2: _t->returnMainwindow(); break;
         case 3: _t->initEnd(); break;
         case 4: _t->onDropAnimationFinished(); break;
         case 5: _t->onEliminateAgain(); break;
-        case 6: _t->on_pushButton_clicked(); break;
-        case 7: _t->on_pushButton_3_clicked(); break;
+        case 6: _t->on_pushButton_3_clicked(); break;
+        case 7: _t->on_returnFromPauseToMainMenu(); break;
+        case 8: _t->resume(); break;
         default: ;
         }
     } else if (_c == QMetaObject::IndexOfMethod) {
@@ -148,14 +154,14 @@ void Game::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void *
         }
         {
             using _t = void (Game::*)();
-            if (_t _q_method = &Game::returnMainwindow; *reinterpret_cast<_t *>(_a[1]) == _q_method) {
+            if (_t _q_method = &Game::initEndSignal; *reinterpret_cast<_t *>(_a[1]) == _q_method) {
                 *result = 1;
                 return;
             }
         }
         {
             using _t = void (Game::*)();
-            if (_t _q_method = &Game::initEndSignal; *reinterpret_cast<_t *>(_a[1]) == _q_method) {
+            if (_t _q_method = &Game::returnMainwindow; *reinterpret_cast<_t *>(_a[1]) == _q_method) {
                 *result = 2;
                 return;
             }
@@ -183,13 +189,13 @@ int Game::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 8)
+        if (_id < 9)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 8;
+        _id -= 9;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 8)
+        if (_id < 9)
             *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
-        _id -= 8;
+        _id -= 9;
     }
     return _id;
 }
@@ -201,13 +207,13 @@ void Game::eliminateAgainSignal()
 }
 
 // SIGNAL 1
-void Game::returnMainwindow()
+void Game::initEndSignal()
 {
     QMetaObject::activate(this, &staticMetaObject, 1, nullptr);
 }
 
 // SIGNAL 2
-void Game::initEndSignal()
+void Game::returnMainwindow()
 {
     QMetaObject::activate(this, &staticMetaObject, 2, nullptr);
 }
